8503d72460caa91f28093232e29db7ee67c7fcb8
Rupam Hari <hrupam55@gmail.com>
2022-06-04 21:16:18 +0530

    added guide

---
+++ smart_contract/hardhatConfigDetails.js
@@ -0,0 +1,9 @@
+// change this according to your local machine
+
+exports.rinkeby = {
+  alchemyUrl:
+    "https://eth-rinkeby.alchemyapi.io/v2/SmZXyVSH9eTg_ldKp8t_2T0IoyQ-dS7G",
+  accountsPrivateKeys: [
+    "92afda326719096ac8060fbeb9228c93419b128a6e5e3788bf10966e09c731bd",
+  ],
+};
